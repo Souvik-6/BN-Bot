373b05abcba86c9aedec706de93ce563eb9b36eb
Souvik G <souvik2u@gmail.com>
2024-11-14 14:52:30 +1100

    Update Assistant-GPT.py

--- Assistant-GPT.py
+++ Assistant-GPT.py
@@ -11,7 +11,8 @@ from streamlit_feedback import streamlit_feedback
 from streamlit_option_menu import option_menu
 
 # Load environment variables
-load_dotenv() = st.secrets()
+os.getenv['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+os.getenv['ASSISTANT_ID'] = st.secrets['ASSISTANT_ID']
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
